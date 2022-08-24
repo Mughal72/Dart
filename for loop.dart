fdbcc2aa808580892a640d9c5d36b900e3b228d1
Anber sattar <82819709+Mughal72@users.noreply.github.com>
2022-08-24 12:37:33 +0500

    Create for loop.dart

---
+++ for loop.dart
@@ -0,0 +1,28 @@
+//for loop
+
+void main(){
+
+// for loop
+  
+  for(int i = 0;  i<=10; i++){
+    
+//     find the even number btewn 1 to 10
+    
+    if (i % 2 == 0){
+
+    print(i);
+      
+     
+    }
+}
+//    for ...in loop
+  
+  List planetList = ["Mercury", "Jupiter", "Earth" , "Mars"];
+  
+  
+  for(String planet in planetList){
+    
+    print(planet);
+}
+
+}
