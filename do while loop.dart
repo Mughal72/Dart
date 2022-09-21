20fd7f128071c121aa0ba6a206c6a033dc336a41
Anber sattar <82819709+Mughal72@users.noreply.github.com>
2022-09-21 14:09:16 +0500

    Create do while loop.dart

---
+++ do while loop.dart
@@ -0,0 +1,18 @@
+
+//dowhile loop
+void main(){
+
+  //WAP to find even numbers between 1 to 10
+  
+  int i = 1;
+  do{
+    if(i%2 == 0){
+
+     print(i);
+    }
+
+ 
+  i++;
+  }while(i<=10);
+}
+//code exeute -> increament -> condition check 
